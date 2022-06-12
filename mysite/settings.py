d7b2ca2c1730fe89c671ec8bd3f49b430637c213
VivekPamnani <vivek.pamnani@research.iiit.ac.in>
2022-06-12 21:36:54 +0530

    smtp2

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,9 +132,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-# EMAIL_HOST_USER = "vivek.pamnani.iiitresearch@gmail.com"
 EMAIL_HOST_USER = "vivpamnani19990429@gmail.com"
-# EMAIL_HOST_PASSWORD = "R@taGooF"
 EMAIL_HOST_PASSWORD = ""
 
 
