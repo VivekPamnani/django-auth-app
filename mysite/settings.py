f83af1c2453c21aad1dd54d3f00579b2e533e0b5
VivekPamnani <vivek.pamnani@research.iiit.ac.in>
2022-06-14 13:50:03 +0530

    auto_email in progress; to fix cronjob

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 CRONJOBS = [
-    ('* * * * *', 'mysite.user.cron.auto_email()', '>> ' + os.path.join(BASE_DIR,'log/debug7.log' + ' 2>&1 '))
+    ('* * * * *', 'user.cron_alt.auto_email')
 ]
 
 
@@ -129,11 +129,11 @@ STATICFILES_DIRS = [
 
 STATIC_URL = 'static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "vivpamnani19990429@gmail.com"
-EMAIL_HOST_PASSWORD = ""
+EMAIL_HOST_USER = "vivek.pamnani.iiit.research@outlook.com"
+EMAIL_HOST_PASSWORD = "ofiljwpvhwgbljlg"
 
 
 # Default primary key field type
