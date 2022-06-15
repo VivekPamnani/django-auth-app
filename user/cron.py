95ba660db71a0704addb9787d5d207142a028457
VivekPamnani <vivek.pamnani@research.iiit.ac.in>
2022-06-15 20:20:37 +0530

    added missing files

---
+++ user/cron.py
@@ -0,0 +1,25 @@
+from django.core.mail import send_mail
+import datetime
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+def auto_email():
+    user_list = User.objects.all()
+    to_addr = []
+    for user in user_list:
+        if user.username != 'admin':
+            rem_time = user.participant.last_visit + datetime.timedelta(days=14) - timezone.now()
+            delta_last_email = (timezone.now() - user.participant.last_email)
+            if(delta_last_email > datetime.timedelta(days=14)):
+                if(rem_time <= datetime.timedelta()):
+                    to_addr.append(user.email)
+                    user.participant.last_email = timezone.now()
+    # to_addr = [i.email for i in user_list]
+    send_mail('testing django auto-mail', 
+        'message', 
+        'vivek.pamnani.iiit.research@outlook.com', 
+        to_addr,
+        fail_silently=True)
+
+if __name__ == "__main__":
+    auto_email()
---
+++ user/cron_alt.py
@@ -0,0 +1,66 @@
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import smtplib
+from django.contrib.auth.models import User
+from unicodedata import name
+
+
+# send our email message 'msg' to our boss
+def message(subject="Python Notification", text="abcd"):
+	
+	# build message contents
+    msg = MIMEMultipart()
+	# Add Subject
+    msg['Subject'] = subject
+    # msg['To'] = 'vivpamnani19990429@gmail.com'
+    # msg['From'] = 'vivek.pamnani.iiit.research@outlook.com'
+	# Add text contents
+    msg.attach(MIMEText(text))
+
+	# Check if we have anything
+	# given in the img parameter
+	
+	# We do the same for
+	# attachments as we did for images
+			
+    return msg
+
+def auto_email():
+    # try:
+    smtp = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+    smtp.ehlo()
+    smtp.starttls()
+
+    # Login with your email and password
+    smtp.login(user="vivek.pamnani.iiit.research@outlook.com", password="vhyrkkrjiacjorzk")
+
+    # Call the message function
+    msg = message("Good!", "Hi there!")
+
+    # Make a list of emails, where you wanna send mail
+    # to = ["vivpamnani19990429@gmail.com", "vivek.pamnani.iiit.research@outlook.com"]
+    # to = ['gameidvivek@gmail.com', 'vivpamnani19990429@gmail.com', 'vivekpamnani04@gmail.com', 'vivek.pamnani@research.iiit.ac.in']
+    to = [i.email for i in User.objects.all()]
+
+
+    # Provide some data to the sendmail function!
+    smtp.sendmail("vivek.pamnani.iiit.research@outlook.com", to, msg.as_string())
+    # smtp.send_message(msg)
+    # Finally, don't forget to close the connection
+    smtp.quit()
+    # except: 
+        # print("Connection Error")
+    # Opening a file
+    try:
+        file1 = open('myfile.txt', 'x')
+    except:
+        file1 = open('myfile.txt', 'a')
+    L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
+    s = "Hello\n"
+    file1.write(s)
+    file1.close()
+    print("Hello There")
+
+
+if __name__ == "__main__":
+     auto_email()
