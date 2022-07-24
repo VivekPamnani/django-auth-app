bc53285f2a75f36ebec0f9f21ea6f71ea88da5af
VivekPamnani <vivek.pamnani@research.iiit.ac.in>
2022-07-24 16:41:01 +0530

    updated cron.py logic

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Note:
 * How to deploy static files: https://docs.djangoproject.com/en/4.0/howto/static-files/deployment/
 * Login required decorator
 * BASE_DIR in settings.py for STATICFILES
-* select id, username, sessions_completed, last_visit, last_email, visit_time_1, visit_time_2, visit_time_3, visit_time_4, visit_time_5, visit_time_6, is_verified from auth_user inner join user_participant on auth_user.id = user_participant.user_id;
+* select auth_user.id, username, email, sessions_completed, last_visit, last_email, visit_time_1, visit_time_2, visit_time_3, visit_time_4, visit_time_5, visit_time_6, is_verified from auth_user inner join user_participant on auth_user.id = user_participant.user_id;
 * Find a way to set session number in Psytoolkit (1 through 6) such that the output data reflects that.
 * Do something about naive datetime.datetime.now()
 
@@ -18,6 +18,10 @@ Note:
 
 *Note that you need Python3.10 for it to work.*
 
+## Crontab
+For crontab, make sure the `cron` and `atd` services are running.
+1. sudo service cron start
+2. sudo service atd start
 
 ### To do:
 1. instructions
--- user/cron.py
+++ user/cron.py
@@ -4,22 +4,33 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 def auto_email():
+    # myfile = open("test.txt", "a")
+    # myfile.write("\nexec\n")
     user_list = User.objects.all()
     to_addr = []
+    body = []
     for user in user_list:
         if user.username != 'admin' and user.participant.is_verified:
             rem_time = user.participant.last_visit + datetime.timedelta(days=14) - timezone.now()
             delta_last_email = (timezone.now() - user.participant.last_email)
             if(delta_last_email > datetime.timedelta(days=14)):
-                if(rem_time <= datetime.timedelta()):
+                if(rem_time <= datetime.timedelta() and user.participant.sessions_completed < 6):
                     to_addr.append(user.email)
+                    body.append("Hi, " + user.username + "! This is an email reminder for the follow-up survey. You have completed " + str(user.participant.sessions_completed) + " out of 6 sessions. Please visit the website to earn and participate: http://covidresearch.pythonanywhere.com/user/login/")
                     user.participant.last_email = timezone.now()
+                    user.save()
     # to_addr = [i.email for i in user_list]
-    send_mail('testing django auto-mail', 
-        'message', 
-        'vivek.pamnani.iiit.research@outlook.com', 
-        to_addr,
-        fail_silently=True)
+    # myfile.write(",".join(to_addr))
+    # myfile.write("\n")
+    # myfile.write("\n".join(body))
+    for addr in to_addr:
+        send_mail('Reminder for your next session.', 
+            body[to_addr.index(addr)], 
+            'vivek.pamnani.iiit.research@outlook.com', 
+            [addr],
+            fail_silently=True)
+
+    # myfile.close()
 
 if __name__ == "__main__":
     auto_email()
