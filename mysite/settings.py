bba1956ddfdce70112936bd64078337916b4b492
VivekPamnani <vivek.pamnani@research.iiit.ac.in>
2022-08-05 17:06:10 +0530

    heroku test

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-cmn*=x-kc3*cjtu+o*i^+d*t^3bq!m^fm5r#iy(rb-v6@0m#f3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['covidresearch.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['covidresearch.pythonanywhere.com', '127.0.0.1', 'imwbs.herokuapp.com']
 
 
 # Application definition
