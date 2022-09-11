b1e250754f018d63f398c8c9c93f106d4fb38456
VivekPamnani <vivek.pamnani@research.iiit.ac.in>
2022-09-11 15:05:40 +0530

    final script.py

--- script.py
+++ script.py
@@ -34,21 +34,21 @@ def auto_email():
                 #         [to_addr[-1]],
                 #         fail_silently=True)
 
-            # 2 DAYS BEFORE
-            if( rem_time <= datetime.timedelta(days=2) and 
-                rem_time >= datetime.timedelta(days=1) and 
-                user.participant.sessions_completed < 6 and 
-                delta_last_email > datetime.timedelta(days=2)
-            ):
-                to_addr.append(user.email)
-                body.append("Hi, " + user.username + "! This is an email reminder for the upcoming follow-up session. You have completed " + str(user.participant.sessions_completed) + " out of 6 sessions. Your reward for completing your next session is ₹" + amounts[user.participant.sessions_completed+1] + "! Please visit the website AFTER 2 days to earn and participate: https://imwbs.pythonanywhere.com/user/login/ . We suggest you block your calendar about 2 days from now for a 30 minute slot.")
-                user.participant.last_email = timezone.now()
-                user.save()
-                send_mail('Your next session is 2 days away.', 
-                    body[-1], 
-                    str(env('smtp_mail')), 
-                    [to_addr[-1]],
-                    fail_silently=True)
+            # # 2 DAYS BEFORE
+            # if( rem_time <= datetime.timedelta(days=2) and 
+            #     rem_time >= datetime.timedelta(days=1) and 
+            #     user.participant.sessions_completed < 6 and 
+            #     delta_last_email > datetime.timedelta(days=2)
+            # ):
+            #     to_addr.append(user.email)
+            #     body.append("Hi, " + user.username + "! This is an email reminder for the upcoming follow-up session. You have completed " + str(user.participant.sessions_completed) + " out of 6 sessions. Your reward for completing your next session is ₹" + amounts[user.participant.sessions_completed+1] + "! Please visit the website AFTER 2 days to earn and participate: https://imwbs.pythonanywhere.com/user/login/ . We suggest you block your calendar about 2 days from now for a 30 minute slot.")
+            #     user.participant.last_email = timezone.now()
+            #     user.save()
+            #     send_mail('[Indian Mental Wellbeing Study] Your next session is 2 days away.', 
+            #         body[-1], 
+            #         str(env('smtp_mail')), 
+            #         [to_addr[-1]],
+            #         fail_silently=True)
 
             # 1 DAY BEFORE
             if( rem_time <= datetime.timedelta(days=1) and 
@@ -60,7 +60,7 @@ def auto_email():
                 body.append("Hi, " + user.username + "! This is an email reminder for the upcoming follow-up session. You have completed " + str(user.participant.sessions_completed) + " out of 6 sessions. Your reward for completing your next session is ₹" + amounts[user.participant.sessions_completed+1] + "! Please visit the website TOMORROW to earn and participate: https://imwbs.pythonanywhere.com/user/login/")
                 user.participant.last_email = timezone.now()
                 user.save()
-                send_mail('Your next session is tomorrow.', 
+                send_mail('[Indian Mental Wellbeing Study] Your next session is tomorrow.', 
                     body[-1], 
                     str(env('smtp_mail')), 
                     [to_addr[-1]],
@@ -76,31 +76,63 @@ def auto_email():
                 body.append("Hi, " + user.username + "! This is an email reminder for the upcoming follow-up session. You have completed " + str(user.participant.sessions_completed) + " out of 6 sessions. Your reward for completing your next session is ₹" + amounts[user.participant.sessions_completed+1] + "! Please visit the website any time TODAY to earn and participate: https://imwbs.pythonanywhere.com/user/login/")
                 user.participant.last_email = timezone.now()
                 user.save()
-                send_mail('Your next session is TODAY.', 
+                send_mail('[Indian Mental Wellbeing Study] Your next session is TODAY.', 
                     body[-1], 
                     str(env('smtp_mail')), 
                     [to_addr[-1]],
                     fail_silently=True)               
 
-            # 1 DAY AFTER
-            if( rem_time < datetime.timedelta(hours=-20) and 
-                rem_time > datetime.timedelta(days=-1, hours=-20) and 
+            # # 1 DAY AFTER
+            # if( rem_time < datetime.timedelta(hours=-20) and 
+            #     rem_time > datetime.timedelta(days=-1, hours=-20) and 
+            #     user.participant.sessions_completed < 6 and 
+            #     delta_last_email > datetime.timedelta(hours=23)
+            # ):
+            #     to_addr.append(user.email)
+            #     body.append("Hi, " + user.username + "! This is an email reminder for the missed follow-up session. You have completed " + str(user.participant.sessions_completed) + " out of 6 sessions. Your reward for completing your next session is ₹" + amounts[user.participant.sessions_completed+1] + "! Please visit the website any time TODAY to earn and participate: https://imwbs.pythonanywhere.com/user/login/")
+            #     user.participant.last_email = timezone.now()
+            #     user.save()
+            #     send_mail('[Indian Mental Wellbeing Study] Your next session is TODAY.', 
+            #         body[-1], 
+            #         str(env('smtp_mail')), 
+            #         [to_addr[-1]],
+            #         fail_silently=True)  
+
+            # # 3 DAYS AFTER
+            # if( rem_time < datetime.timedelta(days=-2, hours=-20) and 
+            #     rem_time > datetime.timedelta(days=-3, hours=-20) and 
+            #     user.participant.sessions_completed < 6 and 
+            #     delta_last_email > datetime.timedelta(hours=24)
+            # ):
+            #     to_addr.append(user.email)
+            #     body.append("Hi, " + user.username + "! This is an email reminder for the missed follow-up session. You have completed " + str(user.participant.sessions_completed) + " out of 6 sessions. Your reward for completing your next session is ₹" + amounts[user.participant.sessions_completed+1] + "! Please visit the website any time TODAY to earn and participate: https://imwbs.pythonanywhere.com/user/login/")
+            #     user.participant.last_email = timezone.now()
+            #     user.save()
+            #     send_mail('[Indian Mental Wellbeing Study] Your next session is TODAY.', 
+            #         body[-1], 
+            #         str(env('smtp_mail')), 
+            #         [to_addr[-1]],
+            #         fail_silently=True)   
+
+            # 4 DAYS AFTER
+            if( rem_time < datetime.timedelta(days=-3, hours=-20) and 
+                rem_time > datetime.timedelta(days=-4, hours=-20) and 
                 user.participant.sessions_completed < 6 and 
-                delta_last_email > datetime.timedelta(hours=23)
+                delta_last_email > datetime.timedelta(hours=24)
             ):
                 to_addr.append(user.email)
                 body.append("Hi, " + user.username + "! This is an email reminder for the missed follow-up session. You have completed " + str(user.participant.sessions_completed) + " out of 6 sessions. Your reward for completing your next session is ₹" + amounts[user.participant.sessions_completed+1] + "! Please visit the website any time TODAY to earn and participate: https://imwbs.pythonanywhere.com/user/login/")
                 user.participant.last_email = timezone.now()
                 user.save()
-                send_mail('Your next session is TODAY.', 
+                send_mail('[Indian Mental Wellbeing Study] Your next session is TODAY.', 
                     body[-1], 
                     str(env('smtp_mail')), 
                     [to_addr[-1]],
-                    fail_silently=True)  
-
-            # 3 DAYS AFTER
-            if( rem_time < datetime.timedelta(days=-2, hours=-20) and 
-                rem_time > datetime.timedelta(days=-3, hours=-20) and 
+                    fail_silently=True)   
+            
+            # 5 DAYS AFTER
+            if( rem_time < datetime.timedelta(days=-4, hours=-20) and 
+                rem_time > datetime.timedelta(days=-5, hours=-20) and 
                 user.participant.sessions_completed < 6 and 
                 delta_last_email > datetime.timedelta(hours=24)
             ):
@@ -108,7 +140,7 @@ def auto_email():
                 body.append("Hi, " + user.username + "! This is an email reminder for the missed follow-up session. You have completed " + str(user.participant.sessions_completed) + " out of 6 sessions. Your reward for completing your next session is ₹" + amounts[user.participant.sessions_completed+1] + "! Please visit the website any time TODAY to earn and participate: https://imwbs.pythonanywhere.com/user/login/ . Note that is the last email reminder from our side.")
                 user.participant.last_email = timezone.now()
                 user.save()
-                send_mail('Your next session is TODAY.', 
+                send_mail('[Indian Mental Wellbeing Study] Your next session is TODAY.', 
                     body[-1], 
                     str(env('smtp_mail')), 
                     [to_addr[-1]],
