55175031fd179459d9675b403ec6c6131e1118fb
VivekPamnani <vivek.pamnani@research.iiit.ac.in>
2022-10-18 00:30:10 +0530

    added admin function to reset original users;

--- user/admin.py
+++ user/admin.py
@@ -7,6 +7,10 @@ from .models import participant, codes
 def mark_paid(modeladmin, request, queryset):
     queryset.update(is_paid=True)
 
+@admin.action(description='Mark tested and not color blind')
+def mark_notBlind(modeladmin, request, queryset):
+    queryset.update(is_colorBlind=False, is_colorTested=True)
+
 class CodesAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'is_paid', 'session_num']
     # ordering = ['title']
@@ -14,6 +18,7 @@ class CodesAdmin(admin.ModelAdmin):
 
 class ParticipantsAdmin(admin.ModelAdmin):
     list_display = ['__str__', 'sessions_completed', 'last_visit', 'last_email']
+    actions = [mark_notBlind]
 
 admin.site.register(participant, ParticipantsAdmin)
 admin.site.register(codes, CodesAdmin)
